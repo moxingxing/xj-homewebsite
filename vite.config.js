5406d7b447e172278af9aef48e0feeec138946ae
莫醒醒 <1935241421@qq.com>
2026-04-10 14:56:34 +0800

    更改打包路径

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/livtag/',
   plugins: [
     vue(),
     vueDevTools(),
